b3241ae3b986ba17fa3652494ebe7503f8df304c
Svetlana177 <svetlanarozhkova177@mail.ru>
2021-09-28 09:07:06 +0300

    module2-task1 fix after code review

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 /*Источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 Получение случайного целого числа в заданном интервале, включительно*/
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return ((min < 0 && max < 0) || max <= min) ? 'Недопустимое значение диапазона' :  Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  if (min < 0 || max < 0 || max <= min) {
+    return 'Недопустимое значение диапазона';
+  } else {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
-getRandomIntInclusive(0.1, 1);
+
+getRandomIntInclusive(-0.1, 1);
 
 const test = 'Hello, World!';
-function getMaxStringLength(str, maxLength = 140) {
-  return str.length < maxLength;
+function isStringValid(str, maxLength = 140) {
+  return str.length <= maxLength;
 }
 
-getMaxStringLength(test);
+isStringValid(test);
